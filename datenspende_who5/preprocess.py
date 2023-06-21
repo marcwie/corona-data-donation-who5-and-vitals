3d3f1f3e0781b4201c90926e189bd837b7061235
Marc Wiedermann <marcwiedermann@posteo.de>
2023-06-21 16:11:24 +0200

    rewrite preprocessing

--- datenspende_who5/preprocess.py
+++ datenspende_who5/preprocess.py
@@ -1,84 +1,107 @@
 import pandas as pd
 from pathlib import Path
-from datetime import date, datetime
+import numpy as np
 
 
 Path("data/02_processed").mkdir(parents=True, exist_ok=True)
+ZIP_TO_NUTS = 'data/00_external/pc2020_DE_NUTS-2021_v3.0.csv'
 
 
+def add_date_column(df):
+    df['date'] = pd.to_datetime(df.created_at // 1000 // 60 // 60 // 24, unit='D')
+    
+def drop_duplicate_entries(df):
+    df.drop_duplicates(subset=['user_id', 'date', 'question', 'choice_id'], inplace=True)
+    df.drop_duplicates(subset=['user_id', 'date', 'question'], keep=False, inplace=True)
+    
+def drop_creation_time_and_description(df):
+    df.drop(columns=['created_at', 'description'], inplace=True)
+
+def put_response_to_each_question_as_column(df):
+    df = df.set_index(['user_id', 'date', 'question']).unstack()
+    return df
+
+def remove_incomplete_responses(df):
+    df.dropna(inplace=True)
+
+def simplify_column_names(df):
+    df.columns = df.columns.droplevel(0)
+    df.columns.names = [None]
+    df.rename(columns={49: 'q49', 50: 'q50', 54: 'q54', 55: 'q55', 56: 'q56'}, inplace=True)
+    
+def set_user_and_date_as_column(df):
+    df.reset_index(inplace=True)
+    df.rename(columns={'user_id': 'userid'}, inplace=True)
+    
 def preprocess_survey_data(input_file):
-
-    # Load raw data
+    
     df = pd.read_feather(input_file)
 
-    # Create date column
-    df['date'] = pd.to_datetime(df.created_at, unit='ms').dt.date
-    df.drop(columns='created_at', inplace=True)
+    add_date_column(df)
+    drop_duplicate_entries(df)
+    drop_creation_time_and_description(df)
 
-    # Drop entries from before the launch of the surveys
-    df = df[df.date >= date(2021, 10, 21)]
+    df = put_response_to_each_question_as_column(df)
 
-    # Drop sessions where users did not respons to all five wellbeing questions or answered multiple times
-    g = df.groupby(['user_id', 'date']).question.count().reset_index()
-    g = g.query('question != 5').drop(columns='question')
-
-    df = pd.merge(df, g, on=['user_id', 'date'], how="outer", indicator=True)
-    df = df.query('_merge=="left_only"').drop(columns='_merge')
-
-    # Compute and add average wellbeing
-    total = df.groupby(['user_id', 'date']).choice_id.mean().reset_index()
-    total['question'] = 60
-    total['description'] = 'Mittleres Wohlbefinden'
-
-    df = pd.concat([df, total], ignore_index=True)
-
-    # Clean up the data frame
-    df = df[['user_id', 'date', 'question', 'choice_id', 'description']]
-    df.sort_values(by=['user_id', 'date'], inplace=True)
-    df.reset_index(inplace=True, drop=True)
-    df.date = pd.to_datetime(df.date)
+    remove_incomplete_responses(df)
+    simplify_column_names(df)
+    set_user_and_date_as_column(df)    
     
-    df.to_feather("data/02_processed/who5_responses.feather")
-
-
-def preprocess_vitals(input_file):
+    df['total_wellbeing'] = df[['q49', 'q50', 'q54', 'q55', 'q56']].mean(axis=1)
     
-    # Load data
+    df.to_feather("data/02_processed/surveys.feather")
+
+
+def preprocess_vital_data(input_file):
+    
     df = pd.read_feather(input_file)
-    df.rename(columns={'userid': 'user_id'}, inplace=True)
-
-    # Compute sleep onset and offset
-    mask = df.vitalid.isin([52, 53])
-    values = (pd.to_datetime(df[mask].value, unit='s') - df[mask].date) / pd.Timedelta(hours=1)
-    df.loc[mask, 'value'] = values
     
-    # Remove implausible values for onset and offset
-    invalid = (df.vitalid == 52) & (~df.value.between(-12, 12))
-    df = df[~invalid]
+    # Put vital data as columns
+    df = df.set_index(['userid', 'date', 'deviceid', 'vitalid']).unstack()
+    df.columns = df.columns.droplevel(0)
+    df.columns.names = [None]
+    df.columns = [f'v{entry}' for entry in df.columns]
+    
+    df.reset_index(inplace=True)
 
-    invalid = (df.vitalid == 53) & (~df.value.between(0, 18))
-    df = df[~invalid]
+    # Compute onset and offset
+    df['v52'] = (pd.to_datetime(df['v52'], unit='s') - df['date']) / pd.Timedelta(hours=1)
+    df['v53'] = (pd.to_datetime(df['v53'], unit='s') - df['date']) / pd.Timedelta(hours=1)
     
     # Correct for DST
-    dst_2021 = df.vitalid.isin([52, 53]) & (df.date < datetime(2021, 10, 31))
-    dst_2022 = df.vitalid.isin([52, 53]) & (df.date > datetime(2022, 3, 27)) & (df.date < datetime(2022, 10, 30))
+    df.loc[(df.date <= '2021-10-31') | df.date.between('2022-03-28', '2022-10-30'), 'v52'] += 1 
+    df.loc[(df.date <= '2021-11-01') | df.date.between('2022-03-28', '2022-10-31'), 'v53'] += 1 
+    
+    # Remove dates past the end of Datenspende
+    df = df[df.date <= '2022-12-31']
+    
+    # Remove Apple sleep data
+    df.loc[df.deviceid == 6, ['v43', 'v52', 'v53']] = np.nan
+    
+    # Remove outliers
+    vmin = df.quantile(.025)
+    vmax = df.quantile(.975)
 
-    df.loc[dst_2021 | dst_2022, 'value'] += 1
+    for vital in ['v65', 'v9', 'v43', 'v52', 'v53']:
+        df.loc[~df[vital].between(vmin[vital], vmax[vital]), vital] = np.nan
     
-    # Compute 28-day rolling average
-    #df = df.set_index('date').groupby(['user_id', 'vitalid']).rolling('28D', min_periods=7).mean().dropna()
-    df.reset_index(inplace=True)
+    # Add boolean variable for weekends
+    df['weekend'] = df.date.dt.dayofweek >= 5
     
-    # Clean up the data frame
-    df.date = pd.to_datetime(df.date).dt.date
-    
+    df.reset_index(drop=True, inplace=True)
     df.to_feather("data/02_processed/vitals.feather")
 
 
 def preprocess_users(input_file):
-
+    
     df = pd.read_feather(input_file)
-    df.drop(columns='creation_timestamp', inplace=True)
+    plz = pd.read_csv(ZIP_TO_NUTS, sep=';')
+
+    plz.NUTS3 = plz.NUTS3.str.replace('\'', '')
+    plz.CODE = plz.CODE.str.replace('\'', '')
+
+    df = pd.merge(df, plz, left_on='zip_5digit', right_on='CODE', how='left')
+    df.drop(columns=['creation_timestamp', 'CODE'], inplace=True)
 
     df.to_feather("data/02_processed/users.feather")
 
@@ -86,7 +109,7 @@ def preprocess_users(input_file):
 def main():
 
     preprocess_survey_data('data/01_raw/who5_responses.feather')
-    preprocess_vitals('data/01_raw/vitals.feather')
+    preprocess_vital_data('data/01_raw/vitals.feather')
     preprocess_users('data/01_raw/users.feather')
 
 if __name__ == "__main__":
